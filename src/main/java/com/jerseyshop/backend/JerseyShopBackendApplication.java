41004fc587503d17ea358f6ec86a275aaadf1738
bakhytzhan <bakhytzhanabdilmazhit@gmail.com>
2025-06-11 11:37:06 +0500

    initial commit

---
+++ src/main/java/com/jerseyshop/backend/JerseyShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jerseyshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseyShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JerseyShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jerseyshop/backend/JerseyShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerseyshop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseyShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
